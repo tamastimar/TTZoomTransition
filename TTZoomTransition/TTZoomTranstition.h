c233501b7671815b3395362e7e63bf0c9577938c
tamastimar <mail@tamastimar.com>
2015-03-15 20:35:29 +0100

    NS_DESIGNATED_INITIALIZER workaround for iOS 7

--- TTZoomTransition/TTZoomTranstition.h
+++ TTZoomTransition/TTZoomTranstition.h
@@ -23,6 +23,15 @@
 
 #import <UIKit/UIKit.h>
 
+// workaround for iOS 7
+#ifndef NS_DESIGNATED_INITIALIZER
+#if __has_attribute(objc_designated_initializer)
+#define NS_DESIGNATED_INITIALIZER __attribute((objc_designated_initializer))
+#else
+#define NS_DESIGNATED_INITIALIZER
+#endif
+#endif
+
 @interface TTZoomTranstition : NSObject <UIViewControllerAnimatedTransitioning>
 
 /**
