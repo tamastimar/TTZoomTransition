172041ce0c046a25e1f778b220caa8eda2b5020c
tamastimar <mail@tamastimar.com>
2015-10-25 17:15:14 +0100

    Updating pods

--- Example/Pods/Target Support Files/Pods-TTZoomTransitionExampleTests-FBSnapshotTestCase/Pods-TTZoomTransitionExampleTests-FBSnapshotTestCase-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TTZoomTransitionExampleTests_FBSnapshotTestCase : NSObject
-@end
-@implementation PodsDummy_Pods_TTZoomTransitionExampleTests_FBSnapshotTestCase
-@end
